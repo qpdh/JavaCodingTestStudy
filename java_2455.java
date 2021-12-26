6ab6d519001ef254594b5a922768c304e91d0e36
qpdh <74190329+qpdh@users.noreply.github.com>
2021-12-26 12:29:32 +0900

    Create java_2455.java

---
+++ java_2455.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class java_2455 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int result = 0;
+        int max = 0;
+
+        for (int i = 0; i < 4; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int out = Integer.parseInt(st.nextToken());
+            int in = Integer.parseInt(st.nextToken());
+
+            result -= out;
+            result += in;
+
+            if (max < result) {
+                max = result;
+            }
+        }
+
+        bw.write(max + "");
+
+        br.close();
+        bw.close();
+    }
+}
