c664986099288e45794b1fb5b0ddf9db27cd185b
qpdh <74190329+qpdh@users.noreply.github.com>
2022-08-21 10:43:42 +0900

    Create java_10166.java

---
+++ java_10166.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class java_10166 {
+    static int euclid(int a, int b) {
+        while (b != 0) {
+            int tmp = a;
+            a = b;
+            b = tmp % b;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int D1 = Integer.parseInt(st.nextToken());
+        int D2 = Integer.parseInt(st.nextToken());
+
+        int result = 0;
+
+        boolean map[][] = new boolean[D2 + 1][D2 + 1];
+
+        for (int i = D1; i < D2 + 1; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                // 최소 공배수
+                int gcd = euclid(i, j);
+                // 분모
+                int a = (int) j / gcd;
+                // 분자
+                int b = (int) i / gcd;
+                if (!map[a][b]) {
+                    map[a][b] = true;
+                    result++;
+                }
+            }
+        }
+
+        bw.write(result + "\n");
+
+        br.close();
+        bw.close();
+    }
+}
